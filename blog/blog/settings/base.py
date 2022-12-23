804ddb85db602a5dbbd187aa014e0e19e4ea3109
FabiSZ12 <96397495+FabiSZ12@users.noreply.github.com>
2022-12-22 22:13:52 -0300

    Update base.py

--- blog/blog/settings/base.py
+++ blog/blog/settings/base.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT = (os.path.join(os.path.dirname(BASE_DIR), 'static'),)
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR), 'static'),)
 
@@ -128,5 +128,3 @@ MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATIC_ROOT = os.path.join(BASE_DIR, '../static/')
